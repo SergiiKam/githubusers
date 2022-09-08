86b1a76012271c6f9c40921668c04b30fa1833e0
Sergii Kamyshev <oleg.biloy@pinngle.me>
2022-09-08 17:03:22 +0200

    comment delete, from MainActivity.kt

--- app/src/main/java/com/example/githubusers/MainActivity.kt
+++ app/src/main/java/com/example/githubusers/MainActivity.kt
@@ -1,14 +1,12 @@
 package com.example.githubusers
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.githubusers.databinding.ActivityMainBinding
-import com.example.githubusers.screens.main.UsersListFragment
 import dagger.hilt.android.AndroidEntryPoint
-import timber.log.Timber
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -21,16 +19,11 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        supportFragmentManager
-            .beginTransaction()
-            .add(R.id.fragmentContainerView, UsersListFragment())
-            .commit()
-
-        //setupActionBarWithNavController(findNavController(R.id.fragmentContainerView))
+        setupActionBarWithNavController(findNavController(R.id.fragmentContainerView))
     }
 
-//    override fun onSupportNavigateUp(): Boolean {
-//        val navController : NavController = findNavController(R.id.fragmentContainerView)
-//        return navController.navigateUp() || super.onSupportNavigateUp()
-//    }
+    override fun onSupportNavigateUp(): Boolean {
+        val navController: NavController = findNavController(R.id.fragmentContainerView)
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
 }
